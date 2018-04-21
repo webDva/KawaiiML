3e7140420374122e77d1a50f8b782ed5b19931a1
webdva <webNoDva@gmail.com>
2018-04-21 16:16:45 -0400

    going to implement k-means clustering

---
+++ kawaiiml/algorithms/kmeansclustering.py
@@ -0,0 +1,13 @@
+class KMeansClustering:
+    """Class for performing k-means clustering on a set of observations."""
+
+    def __init__(self, observations = None):
+        self.observations = observations
+
+    def hard_kmeans(self):
+        """Uses the Lloyod's algorithm to perform hard k-means clustering."""
+        pass
+
+        # assignment step
+
+        # udpate step
